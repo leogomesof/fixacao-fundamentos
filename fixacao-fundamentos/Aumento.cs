c0fb1616e07a5a5a588e3f9f3d48b0b5adef6583
2022102176 <2022102176@LAB2-009>
2023-08-29 19:01:08 -0300

    Adicionar arquivos de projeto.

---
+++ fixacao-fundamentos/Aumento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fixacao_fundamentos
+{
+    public static class Aumento
+    {
+        public static double Calculo()
+        {
+            Console.Write("Digite um valor para Salário: R$");
+            string salario = Console.ReadLine();
+            double valorSalario = double.Parse(salario);
+
+            Console.Write("Digite a porcentagem do Aumento: ");
+            string aumento = Console.ReadLine();
+            double valorAumento = double.Parse(aumento);
+
+            double porcentagem = valorAumento / 100;
+            double reajuste = valorSalario * porcentagem;
+            double valorFinal = valorSalario + reajuste;
+            return valorFinal;
+        }
+    }
+}
---
+++ fixacao-fundamentos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace fixacao_fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O salário final é: " + Aumento.Calculo());
+        }
+    }
+}
